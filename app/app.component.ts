87aa53059b35589a4b9baf7b20d7a1918a01c05e
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-15 21:15:39 +0700

    Use hero detail component and bind selected hero object into its property

--- app/app.component.ts
+++ app/app.component.ts
@@ -19,18 +19,13 @@ const HEROES: Hero[] = [
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-    <label>name: </label>
-    <input [(ngModel)]="hero.name" placeholder="name">
-  </div>
-  <h2>Heroes</h2>
+  <h2>My Hero</h2>
   <ul class="heroes">
   <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
   <span class="badge">{{hero.id}}</span> {{hero.name}}
 </li>
 </ul>
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles: [`
   .selected {
@@ -86,10 +81,10 @@ const HEROES: Hero[] = [
 export class AppComponent {
 
   title = 'Tour of Heroes';
-  
-
-  hero: Hero = { id: 1, name: 'Windstorm' };
   heroes = HEROES;
+  selectedHero: Hero;
 
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }

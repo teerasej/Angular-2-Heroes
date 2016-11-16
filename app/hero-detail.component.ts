c0ad08cb40fd4b10ccb2239caf55170d1098bebb
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-16 15:02:05 +0700

    Separate hero detail component

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { Hero } from './hero';
+
+@Component({
+    selector: 'my-hero-detail',
+    template: `
+        <div *ngIf="hero">
+            <h2>{{name}} details!</h2>
+            <div>
+                <label for="">id: </label>{{hero.id}}
+            </div>
+            <div>
+                <label for="">name: </label>
+                <input [(ngModel)]="hero.name" placeholder="name">
+            </div>
+        </div>
+    `
+})
+
+export class HeroDetailComponent {
+
+    @Input()
+    hero: Hero;
+}

d24ed944ebf88ae3b3197547c54d0326e131427c
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-17 22:00:23 +0700

    Add dashboard and config routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,17 +2,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 // Components
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
 
 // Services
 import { HeroService } from './hero.service';
+
+
+
+
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroDetailComponent, HeroesComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }, 
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,],
   bootstrap: [AppComponent],
   providers: [HeroService],
 

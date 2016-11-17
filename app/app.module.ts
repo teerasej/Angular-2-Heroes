28544615f5819ca8bad8ff6bde9df5c53467a628
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-17 21:38:17 +0700

    Rearrange import for readability

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,20 @@
-import { NgModule }      from '@angular/core';
+// Modules
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
+// Components
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+
+// Services
+import { HeroService } from './hero.service';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [AppComponent, HeroDetailComponent],
-  bootstrap: [AppComponent]
-  
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService],
+
 })
 export class AppModule { }

dfb36edc2c4a345e0e02a2e4b845ee7ab24e85b4
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-18 08:31:08 +0700

    Add parameter route to Hero Detial Component

--- app/app.module.ts
+++ app/app.module.ts
@@ -30,6 +30,10 @@ import { HeroService } from './hero.service';
         component: DashboardComponent
       }, 
       {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }, 
+      {
         path: '',
         redirectTo: '/dashboard',
         pathMatch: 'full'
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,6 +5,13 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes()
+            .then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+
     getHeroes(): Promise<any> {
         return Promise.resolve(HEROES);
     }

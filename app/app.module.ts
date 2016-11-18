a9ba9c7249fa47ea30ebf0a99dd82b63d3dc1841
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-18 22:30:45 +0700

    Add HttpModule to app's module

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 // Components
@@ -20,6 +21,7 @@ import { HeroService } from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [

6122fd5d09d87cf74e1f32b93f1e168d0fa7d1b9
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-18 22:28:22 +0700

    Separate style to CSS file

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
     template: `
   <h1>{{title}}</h1>
@@ -8,7 +9,8 @@ import { Component } from '@angular/core';
      <a routerLink="/heroes">Heroes</a>
    </nav>
   <router-outlet></router-outlet>
-  `
+  `,
+  styleUrls: ['app.component.css']
 })
 
 export class AppComponent {

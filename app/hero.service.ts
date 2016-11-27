5642832dd05bbf9e825c1b7552af6e31b98eb437
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-11-27 09:30:43 +0700

    Add Promise to service

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+import { Headers, Http } from '@angular/http';
 
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
+
+    private heroesUrl = 'app/heroes';
+
+    constructor(private http: Http) {
+
+    }
 
     getHero(id: number): Promise<Hero> {
         return this.getHeroes()
@@ -12,7 +20,15 @@ export class HeroService {
     }
 
 
-    getHeroes(): Promise<any> {
-        return Promise.resolve(HEROES);
+    getHeroes(): Promise<Hero[]> {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 }
